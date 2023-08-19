8ba88102a9a30957e5f8f73b8ac47ee62138c235
unknown <upmajain2305@gmail.com>
2023-08-19 23:26:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Number from "./components/Number";
+import PlusButton from "./components/PlusButton";
+import MinusButton from "./components/MinusButton";
+import CheckEven from "./components/CheckEven";
+import { NumberProvider } from "./context/NumberContext";
+
+const App = () => {
+  return (
+    <NumberProvider>
+      <div className="container p-3">
+        <Number />
+        <PlusButton />
+        <MinusButton />
+        <CheckEven />
+      </div>
+    </NumberProvider>
+  );
+};
+
+export default App;
+123;
---
+++ src/components/MinusButton.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import NumberContext from "../context/NumberContext";
+
+const MinusButton = () => {
+  const { minusNumber } = useContext(NumberContext);
+
+  return (
+    <button
+      className="btn btn-danger rounded-0 w-100 my-2"
+      onClick={minusNumber}
+    >
+      Decrease Value
+    </button>
+  );
+};
+
+export default MinusButton;
